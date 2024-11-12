19ba5ecd1c5222b5d21cf3ef1bdd3304d0cfa189
ChangWoo <kilolo0@naver.com>
2024-11-12 20:32:17 +0900

    [fix] 로고 추가

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -28,10 +28,22 @@ class _SplashscreenState extends ConsumerState<Splashscreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const Text(
+                  '부엉네컷',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.brown),
+                ),
+                const Gap(30),
                 Container(
                     width: 100,
                     height: 100,
-                    decoration: const BoxDecoration(color: Colors.green)),
+                    decoration: const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        image: DecorationImage(
+                            image: AssetImage('assets/logo.png')))),
                 const Gap(30),
                 GestureDetector(
                     onTap: () {
@@ -41,7 +53,7 @@ class _SplashscreenState extends ConsumerState<Splashscreen> {
                               builder: (context) => const Getimagesscreen()));
                     },
                     child: const Text('시작하기',
-                        style: TextStyle(color: Colors.brown, fontSize: 20))),
+                        style: TextStyle(color: Colors.black, fontSize: 20))),
               ],
             ),
           ),
