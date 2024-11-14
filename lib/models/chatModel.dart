dbafb338e3025d9ad8befa145192d9cd20d0d9f9
ChangWoo <kilolo0@naver.com>
2024-11-14 17:07:57 +0900

    [feat] chat model생성 함수 - factory함수로 간편화

--- lib/models/chatModel.dart
+++ lib/models/chatModel.dart
@@ -5,6 +5,17 @@ class Chat {
   Chat({required this.text, required this.time});
 
   factory Chat.fromJson(Map<String, String> json) {
-    return Chat(text: json['text'] ?? '', time: json['time'] ?? 's');
+    return Chat(text: json['text'] ?? '', time: json['time'] ?? '');
   }
+
+  factory Chat.now(String text) {
+    DateTime now = DateTime.now();
+
+    String time =
+        '${now.month}/${now.day} ${(now.hour < 10) ? ('0${now.hour}') : now.hour}:${(now.minute < 10) ? ('0${now.minute}') : now.minute}';
+
+    return Chat(text: text, time: time);
+  }
+
+  // factory Chat.now(String text)
 }
