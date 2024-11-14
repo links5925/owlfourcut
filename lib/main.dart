84f7c1b6b2e013d165a78e0d79bc28e5e883a149
ChangWoo <kilolo0@naver.com>
2024-11-14 19:00:55 +0900

    [feat] firebase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:owlfourcut/firebase_options.dart';
 import 'package:owlfourcut/screens/splashScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase 초기화
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 앱 실행
   runApp(const ProviderScope(child: MyApp()));
 }
 
